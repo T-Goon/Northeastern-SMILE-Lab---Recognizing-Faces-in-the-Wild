bac91135172e14aa7a9bc360a8fab444c28d395a
Timothy Goon <t.kgoonx@gmail.com>
2021-05-07 00:30:38 -0400

    makes positive traingin data correctly

--- final_project.py
+++ final_project.py
@@ -3,6 +3,30 @@ import pandas as pd
 from os import listdir, path
 from PIL import Image
 
+def save_batch(df, i):
+    trainData = [list(), list()]
+    for index, row in df.iterrows():
+        
+        try:
+            imgs_p1 = listdir(row['p1'])
+            imgs_p2 = listdir(row['p2'])
+
+            for file1 in imgs_p1:
+
+                img1 = Image.open(row['p1']+'/'+file1)
+                for file2 in imgs_p2:
+                    img2 = Image.open(row['p2']+'/'+file2)
+
+                    trainData[0].append(np.asarray(img1))
+                    trainData[1].append(np.asarray(img2))
+        except FileNotFoundError:
+            pass
+
+    # save all the images in a numpy file
+    data = np.array(trainData)
+    np.save("training_images{}.npy".format(i), data)
+    pass
+
 # Turns positive training examples into a large numpy array
 # array is saved in training_images.npy
 # Format [person 1 or person 2, 
@@ -28,29 +52,10 @@ def convert_to_numpy():
     df['p2'] = 'data/train/' + df['p2']
 
     # random sample of 20% of the data set
-    # print(df.sample(frac=.2))
-
-    trainData = [list(), list()]
-    for index, row in df.iterrows():
-        # print(row['p1'], row['p2'])
-        try:
-            imgs_p1 = listdir(row['p1'])
-            imgs_p2 = listdir(row['p2'])
-
-            for file1, file2 in zip(imgs_p1, imgs_p2):
-
-                img1 = Image.open(row['p1']+'/'+file1)
-                img2 = Image.open(row['p2']+'/'+file2)
-
-                trainData[0].append(np.asarray(img1))
-                trainData[1].append(np.asarray(img2))
-        except FileNotFoundError:
-            pass
-
-    # save all the images in a numpy file
-    data = np.array(trainData)
-
-    np.save("training_images.npy", data)
+    p = .05
+    for i in range(20):
+        b = df.sample(frac=p)
+        save_batch(b, i)
 
 if __name__ == '__main__':
     if(not path.exists('training_images.npy')):
