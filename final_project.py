51af1bfae22409335132b311459f8127c62432e0
Timothy Goon <t.kgoonx@gmail.com>
2021-05-07 21:03:42 -0400

    make negative samples and downscale images

--- final_project.py
+++ final_project.py
@@ -3,29 +3,67 @@ import pandas as pd
 from os import listdir, path
 from PIL import Image
 
-def save_batch(df, i):
+# creates positive samples from the data set
+def save_positive_samples(df, i):
     trainData = [list(), list()]
+
     for index, row in df.iterrows():
         
         try:
+            # get all images in the 2 directories
             imgs_p1 = listdir(row['p1'])
             imgs_p2 = listdir(row['p2'])
 
+            # all combinations of the images in either directory
             for file1 in imgs_p1:
 
                 img1 = Image.open(row['p1']+'/'+file1)
                 for file2 in imgs_p2:
                     img2 = Image.open(row['p2']+'/'+file2)
 
-                    trainData[0].append(np.asarray(img1))
-                    trainData[1].append(np.asarray(img2))
+                    # resize image and turn into an np array
+                    trainData[0].append(np.asarray(img1.resize((128, 128), Image.ANTIALIAS)))
+                    trainData[1].append(np.asarray(img2.resize((128, 128), Image.ANTIALIAS)))
         except FileNotFoundError:
             pass
 
     # save all the images in a numpy file
     data = np.array(trainData)
-    np.save("training_images{}.npy".format(i), data)
-    pass
+    np.save("training_images_pos{}.npy".format(i), data)
+
+# creates negative samples from the data set
+def save_negative_samples(df, i):
+    trainData = []
+    person1 = []
+    person2 = []
+
+    for index, row in df.iterrows():
+        
+        try:
+            # get all the images in the 2 directories
+            imgs_p1 = listdir(row['p1'])
+            imgs_p2 = listdir(row['p2'])
+
+            # all combinations of the images in either directory
+            for file1 in imgs_p1:
+
+                img1 = Image.open(row['p1']+'/'+file1)
+                for file2 in imgs_p2:
+                    img2 = Image.open(row['p2']+'/'+file2)
+
+                    # resize image and turn into an np array
+                    person1.append(np.asarray(img1.resize((128, 128), Image.ANTIALIAS)))
+                    person2.append(np.asarray(img2.resize((128, 128), Image.ANTIALIAS)))
+        except FileNotFoundError:
+            pass
+
+    np.random.shuffle(person2)
+
+    trainData.append(person1)
+    trainData.append(person2)
+    # save all the images in a numpy file
+    data = np.array(trainData)
+    np.save("training_images_neg{}.npy".format(i), data)
 
 # Turns positive training examples into a large numpy array
 # array is saved in training_images.npy
@@ -53,11 +91,23 @@ def convert_to_numpy():
 
     # random sample of 20% of the data set
     p = .05
-    for i in range(20):
+    for i in range(1):
         b = df.sample(frac=p)
-        save_batch(b, i)
+        save_positive_samples(b, i)
+
+    for i in range(1):
+        b = df.sample(frac=p)
+        save_negative_samples(b, i)
 
 if __name__ == '__main__':
-    if(not path.exists('training_images.npy')):
+    if(not path.exists('training_images_pos0.npy') and 
+        not path.exists('training_image_neg0.npy')):
         convert_to_numpy()
+    else:
+        train_pos = np.load('training_images_pos0.npy')
+        train_neg = np.load('training_images_neg0.npy')
+
+        print(train_pos.shape)
+        print(train_neg.shape)
+        pass
     pass
